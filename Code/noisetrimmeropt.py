7c484c0408a67b272cf3eb284cd5015941a3740a
Casper McTavish <johnwaiton@hotmail.co.uk>
2021-03-03 17:14:01 +0000

    Applied LCMS algorithm to dark count finder. Improved Memory usage of Dark noise tester (noisetrimmeropt.py)
    Also added runtime calculations to determine what sections are least efficient.

--- Code/noisetrimmeropt.py
+++ Code/noisetrimmeropt.py
@@ -7,6 +7,7 @@ import random
 import pyfftw
 import probfit
 import statistics
+from time import process_time
 
 from iminuit import Minuit
 from scipy import signal
@@ -34,7 +35,7 @@ from scipy import stats
 
 
 # Open the data, apply to variable
-file = "E:\PMTsignals\Boulby_107.root"
+file = "E:\PMTsignals\Boulby_78.root"
 
 tree = uproot.open(file)["Tree"]
 branches = tree.arrays()
@@ -61,38 +62,73 @@ y = 1000000
 data = datafull[:y]
 
 
-# Find mean
-meanvals = []
-for i in range(y):
-    datarepeated = data[i]
+# Find mean OBSOLETE, ONLY USE IF USING SIGMAEVENTS CODE
+#meanvals = []
+#for i in range(y):
+#    datarepeated = data[i]
     # Append data values to list
-    meanvals.append(np.mean(datarepeated))
+#    meanvals.append(np.mean(datarepeated))
 
-# Apply baseline subtraction
-scaleddata = fnc.baselinesubtraction(data,meanvals)
+# timing each process
+t0 = process_time()
+
+
+# Apply LCMS baseline & trendline removal
+scaleddata = fnc.LCMSlist(data)
+
+# timing each process
+t1 = process_time()
+totalLCMS = t1-t0
+print("[1/5] LCMS algorithm Applied. Runtime: " + str(totalLCMS) + "s")
+
+
 
 # Butterworth Filter, removing everything above 2000MHz
-filterdata = []
+
+# timing each process
+t2 = process_time()
+
+#filterdata = []
 sos = signal.butter(5,200,'lp',fs=500,output='sos')
 # Applying filter to data
 for i in range(y):
-    filterdata.append(signal.sosfilt(sos,scaleddata[i]))
+    newdata = signal.sosfilt(sos,scaleddata[i])
+    scaleddata[i] = newdata
+    #filterdata.append(signal.sosfilt(sos,scaleddata[i]))
+
+# timing each process
+t3 = process_time()
+totalbutterworth = t3-t2
+print("[2/5] Butterworth Filter Applied. Runtime: " + str(totalbutterworth) + "s")
 
 # Apply rolling mean to data, window of 5 samples currently
-rollingdata = []
-for i in range(len(filterdata)):
-    rollingdata.append(fnc.rollmean(filterdata[i],5))
+
+# timing each process
+t4 = process_time()
+#rollingdata = []
+for i in range(len(scaleddata)):
+    newerdata = fnc.rollmean(scaleddata[i],5)
+    scaleddata[i] = newerdata
+    # rollingdata.append(fnc.rollmean(scaleddata[i],5))
+
+# timing each process
+t5 = process_time()
+totalrolling = t5-t4
+print("[3/5] Rolling Mean Applied. Runtime: " + str(totalrolling) + "s")
 
 # Collect new average values for signal properties calculations
 #fmeanvals, fstdvals, fminvals, fmaxvals, fsigvals, fmedvals = fnc.datacollate(rollingdata, len(rollingdata))
+
+# timing each process
+t6 = process_time()
 
 # To increase speed, moved this such that it is a much shorter and more concise piece of code
 fminvals = []
 fmedvals = []
 
-# Testing range of y values
+# Collecting minimum and median for property evaluation
 for o in range(y):
-    datarepeater = rollingdata[o]
+    datarepeater = scaleddata[o]
     # Append data values to list
     fminvals.append(np.amin(datarepeater))
     fmedvals.append(statistics.median(datarepeater))
@@ -109,20 +145,10 @@ fwhmlst = []
 sgdpthlst = []
 
 
-for n in range(len(rollingdata)):
+for n in range(len(scaleddata)):
 
 
     ####onesigvals = fnc.sigmaevents(rollingdata[n],fmedvals[n],fstdvals[n],sigmaval) # 0 -> fmeanvals[n]
-
-    # FWHM code
-    #FWHMVAL = (fminvals[n]//2)
-    # set to timegate due to inaccuracies in valuation, will be tested visually
-    #fwhmlength = timegate
-    # go through all the values of the fit, and find what values are closest to this.
-    #for j in range(len(onesigvals)):
-        # if rollingdata[i] is larger, and hasn't been detected yet, ignore, if lower and hasn't been detected, mark
-    #    if (rollingdata[n][j] <= FWHMVAL):
-    #        fwhmlength += timegate
 
     # Takes difference from median to minimum value to find maximum of spike
     difference = fmedvals[n] + fminvals[n]
@@ -130,14 +156,14 @@ for n in range(len(rollingdata)):
     # find half minimum (which is half maximum for us)
     halfmin = difference / 2
     # find nearest point within the array to half max(min). RETURNS THE ARRAY ELEMENT NUMBER! NOT THE ARRAY VALUE!
-    nearest = (np.abs(rollingdata[n]-halfmin)).argmin()
+    nearest = (np.abs(scaleddata[n]-halfmin)).argmin()
     # Once spotted, start counting
     # count how many events occur between nearest and minimum
     spotted = 0
     count = 0
-    for k in range(len(rollingdata[n])):
+    for k in range(len(scaleddata[n])):
         # whichever component it comes into contact first, start the count
-        if (rollingdata[n][k] == fminvals[n]) or (rollingdata[n][k] == rollingdata[n][nearest]):
+        if (scaleddata[n][k] == fminvals[n]) or (scaleddata[n][k] == scaleddata[n][nearest]):
             spotted += 1
 
         # if one component has been spotted, but not the other, keep counting
@@ -170,31 +196,14 @@ for n in range(len(rollingdata)):
     #    plt.ylabel("ADC Value")
     #    plt.show()
 
-
-    #plt.plot(time,rollingdata[n], label="Normal")
-    #plt.plot(time,onesigvals, label=str(sigmaval) + " sigma")
-    #plt.plot(time,fwhmlist, label="FWHM")
-    #plt.legend()
-    #plt.title("Butterworth Rolling Filtered Signal with FWHM of " + str(file) + " event " + str(n))
-    #plt.xlabel("Sample Time (ns)")
-    #plt.ylabel("ADC Value")
-    #plt.show()
-
-
-    # continuous fit of crystal ball function
-    #cmean, cvar, cskew, ckurt = stats.crystalball.fit(rollingdata[n], floc=fminvals[n])
-    #print("Crystal ball values: ")
-    #print(cmean, cvar, cskew, ckurt)
-    #print(fminvals[n])
-
-    #plt.plot(time,stats.crystalball.rvs(time, ))
-    #plt.show
-
-
     # Apply to lists
     fwhmlst.append(fwhmlength)
     sgdpthlst.append(fminvals[n])
 
+# timing each process
+t7 = process_time()
+totalproperties = t7-t6
+print("[4/5] Properties Determined. Runtime: " + str(totalproperties) + "s")
 
 # Construct data cutoff point here, remove all values with signal depth > -50 and see how many signals are left
 cutdata = []
@@ -206,14 +215,12 @@ fwhmlowerbound = 7.5
 fwhmupperbound = 20.5
 
 
-for i1 in range(len(rollingdata)):
+for i1 in range(len(scaleddata)):
     if (sgdpthlst[i1] < sgdpthcutoff): #and (fwhmlst[i1] < fwhmupperbound) and (fwhmlst[i1] > fwhmlowerbound):
         #cutdepth.append(sgdpthlst[i1])
         cutdata.append(i1)
-    if ((i1%1000) == 0):
-        print("Event: " + str(i1) + "/100000")
 
-
+print("[5/5] Dark Counts Determined")
 
 # Depth Cut off
 #plt.hist(cutdepth,bins = 25)
@@ -221,7 +228,6 @@ for i1 in range(len(rollingdata)):
 #plt.ylabel("Count")
 #plt.xlabel("Depth (ADC Value)")
 #plt.show()
-
 
 print("Number of signal events detected:")
 print(len(cutdata))
